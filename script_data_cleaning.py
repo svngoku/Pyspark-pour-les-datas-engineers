86f98451fb2b7477b4f39fe2400490fc2ef98c06
chrys fé-marty NIONGOLO <chrysniongolo@gmail.com>
2022-11-13 01:42:12 +0100

    Updating the data cleaning script file

--- script_data_cleaning.py
+++ script_data_cleaning.py
@@ -2,6 +2,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from pyspark.sql.functions import when
 
 spark = SparkSession \
     .builder \
@@ -26,7 +28,6 @@ aa_dfw_df = aa_dfw_df.drop(aa_dfw_df['Destination Airport'])
 
 # Show the DataFrame
 aa_dfw_df.show()
-
 
 # View the row count of df1 and df2
 print("df1 Count: %d" % df1.count())
@@ -64,3 +65,74 @@ voter_df = voter_df.filter(
 
 # Show the distinct VOTER_NAME entries again
 voter_df.select("VOTER_NAME").distinct().show(40, truncate=False)
+
+# Add a new column called splits separated on whitespace
+voter_df = voter_df.withColumn('splits', F.split(voter_df.VOTER_NAME, '\s+'))
+
+# Create a new column called first_name based on the first item in splits
+voter_df = voter_df.withColumn('first_name', voter_df.splits.getItem(0))
+
+# Get the last entry of the splits list and create a column called last_name
+voter_df = voter_df.withColumn('last_name', voter_df.splits.getItem(F.size('splits') - 1))
+
+# Drop the splits column
+voter_df = voter_df.drop('splits')
+
+# Show the voter_df DataFrame
+voter_df.show()
+
+# Add a column to voter_df for any voter with the title **Councilmember**
+voter_df = voter_df.withColumn('random_val',when(voter_df.TITLE == 'Councilmember', F.rand()))
+
+# Show some of the DataFrame rows, noting whether the when clause worked
+voter_df.show()
+
+# Add a column to voter_df for a voter based on their position
+voter_df = voter_df.withColumn('random_val', when(voter_df.TITLE == 'Councilmember', F.rand()) \
+    .when(voter_df.TITLE == 'Mayor', 2) \
+    .otherwise(0))
+
+# Show some of the DataFrame rows
+voter_df.show()
+
+# Use the .filter() clause with random_val
+voter_df.filter(
+    voter_df.random_val == 0
+).show()
+
+def getFirstAndMiddle(names):
+  # Return a space separated string of names
+  return ' '.join(names[:-1])
+
+# Define the method as a UDF
+udfFirstAndMiddle = F.udf(getFirstAndMiddle, StringType())
+
+# Create a new column using your UDF
+voter_df = voter_df.withColumn('first_and_middle_name', udfFirstAndMiddle(voter_df.splits))
+
+# Show the DataFrame
+voter_df.show()
+# +----------+-------------+-------------------+--------------------+----------+---------+---------------------+
+# |      DATE|        TITLE|         VOTER_NAME|              splits|first_name|last_name|first_and_middle_name|
+# +----------+-------------+-------------------+--------------------+----------+---------+---------------------+
+# |02/08/2017|Councilmember|  Jennifer S. Gates|[Jennifer, S., Ga...|  Jennifer|    Gates|          Jennifer S.|
+# |02/08/2017|Councilmember| Philip T. Kingston|[Philip, T., King...|    Philip| Kingston|            Philip T.|
+# |02/08/2017|        Mayor|Michael S. Rawlings|[Michael, S., Raw...|   Michael| Rawlings|           Michael S.|
+# |02/08/2017|Councilmember|       Adam Medrano|     [Adam, Medrano]|      Adam|  Medrano|                 Adam|
+# |02/08/2017|Councilmember|       Casey Thomas|     [Casey, Thomas]|     Casey|   Thomas|                Casey|
+# |02/08/2017|Councilmember|Carolyn King Arnold|[Carolyn, King, A...|   Carolyn|   Arnold|         Carolyn King|
+# |02/08/2017|Councilmember|       Scott Griggs|     [Scott, Griggs]|     Scott|   Griggs|                Scott|
+# |02/08/2017|Councilmember|   B. Adam  McGough| [B., Adam, McGough]|        B.|  McGough|              B. Adam|
+# |02/08/2017|Councilmember|       Lee Kleinman|     [Lee, Kleinman]|       Lee| Kleinman|                  Lee|
+# |02/08/2017|Councilmember|      Sandy Greyson|    [Sandy, Greyson]|     Sandy|  Greyson|                Sandy|
+# |02/08/2017|Councilmember|  Jennifer S. Gates|[Jennifer, S., Ga...|  Jennifer|    Gates|          Jennifer S.|
+# |02/08/2017|Councilmember| Philip T. Kingston|[Philip, T., King...|    Philip| Kingston|            Philip T.|
+# |02/08/2017|        Mayor|Michael S. Rawlings|[Michael, S., Raw...|   Michael| Rawlings|           Michael S.|
+# |02/08/2017|Councilmember|       Adam Medrano|     [Adam, Medrano]|      Adam|  Medrano|                 Adam|
+# |02/08/2017|Councilmember|       Casey Thomas|     [Casey, Thomas]|     Casey|   Thomas|                Casey|
+# |02/08/2017|Councilmember|Carolyn King Arnold|[Carolyn, King, A...|   Carolyn|   Arnold|         Carolyn King|
+# |02/08/2017|Councilmember| Rickey D. Callahan|[Rickey, D., Call...|    Rickey| Callahan|            Rickey D.|
+# |01/11/2017|Councilmember|  Jennifer S. Gates|[Jennifer, S., Ga...|  Jennifer|    Gates|          Jennifer S.|
+# |04/25/2018|Councilmember|     Sandy  Greyson|    [Sandy, Greyson]|     Sandy|  Greyson|                Sandy|
+# |04/25/2018|Councilmember| Jennifer S.  Gates|[Jennifer, S., Ga...|  Jennifer|    Gates|          Jennifer S.|
+# +----------+-------------+-------------------+--------------------+----------+---------+---------------------+
